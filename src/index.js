94ff07308044de06c17df742b0bf56674d78dde6
Desarrollo VL6 <vl.desarrollo6@corporativo.rll>
2025-04-12 14:14:04 -0600

    avance

--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+function Saludo() {
+  return <div>
+    <h1>Soy de la funcion</h1>
+    <p>Parrafo de Saludo</p>
+    </div>
+}
+
+function Nueva() {
+  return <div>
+      <h1>Nueva funcion</h1>
+      <p>Parrafo de Nueva</p>
+    </div>
+}
+
+function User(){
+    const name = 'carlos'
+    return <h1>{ name }</h1>
+}
+
+function Merried(){
+    const merried = true
+
+    return <h1>{merried? 'Estoy casado' : 'No estoy'}</h1>
+}
+
+//Para los objetos
+function Objeto(){
+    const user = {
+        nombre: 'Carlos',
+        apellido: 'Lopez'
+    }
+    return <div>
+        <h1>{user.nombre}</h1>
+        <h1>{user.apellido}</h1>
+        </div>
+}
+
+/* Siempre se necesita un contenedor padre para hacer 
+   el render 
+*/
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <div> 
+    <Saludo/>
+    <Nueva/>
+    <User/>
+    <Merried/>
+    <Objeto/>
+  </div>
+)
